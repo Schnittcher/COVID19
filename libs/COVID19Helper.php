2338b50233d4902fa990fd4ceadf5a633ba2ec9f
Kai Schnittcher <kai.schnittcher@gmail.com>
2021-03-10 09:41:23 +0100

    Debugs hinzugefügt

--- libs/COVID19Helper.php
+++ libs/COVID19Helper.php
@@ -18,9 +18,9 @@ trait COVID19Helper
         $url = 'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/ArcGIS/rest/services/Coronaf%C3%A4lle_in_den_Bundesl%C3%A4ndern/FeatureServer/0/query?where=' . $where . '&outFields=' . $outFields . '&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=false&quantizationParameters=&sqlFormat=none&f=pjson&token=';
 
         $this->SendDebug('Province Request URL', $url, 0);
-
         $dataJSON = file_get_contents($url);
         $data = json_decode($dataJSON, true);
+        $this->SendDebug('API Result', $dataJSON, 0);
         return $data['features'];
     }
 
@@ -29,10 +29,11 @@ trait COVID19Helper
         $where = urlencode($where);
         $outFields = urlencode($outFields);
         $url = 'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=' . $where . '&outFields=' . $outFields . '&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=false&quantizationParameters=&sqlFormat=none&f=pjson&token=';
-        $this->SendDebug('District Request URL', $url, 0);
 
+        $this->SendDebug('District Request URL', $url, 0);
         $dataJSON = file_get_contents($url);
         $data = json_decode($dataJSON, true);
+        $this->SendDebug('API Result', $dataJSON, 0);
         return $data['features'];
     }
 }
