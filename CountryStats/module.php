73eee2ba5b0b2cd8e1edebef836462a68343b964
Kai Schnittcher <kai.schnittcher@gmail.com>
2021-11-19 08:50:38 +0100

    Neu: R-Wert 4 Tage Neu: R-Wert 7 Tage Neu: Delta Fälle Neu: Delta Tote Neu: Delta Genesene

--- CountryStats/module.php
+++ CountryStats/module.php
@@ -12,7 +12,7 @@ require_once __DIR__ . '/../libs/COVID19Helper.php';
             parent::Create();
 
             $this->RegisterPropertyInteger('UpdateInterval', 10);
-            $this->RegisterPropertyString('URL','https://api.corona-zahlen.org');
+            $this->RegisterPropertyString('URL', 'https://api.corona-zahlen.org');
             $this->RegisterTimer('COVID_CountryUpdateStats', 0, 'COVID_updateCountryStats($_IPS[\'TARGET\']);');
         }
 
@@ -29,7 +29,7 @@ require_once __DIR__ . '/../libs/COVID19Helper.php';
 
             $this->RegisterVariableString('country', $this->Translate('Country'), '', 0);
             $this->SetValue('country', 'Deutschland');
-            $this->RegisterVariableInteger('last_update', $this->Translate('Last Update'), '~UnixTimestamp', 8);
+            $this->RegisterVariableInteger('last_update', $this->Translate('Last Update'), '~UnixTimestamp', 15);
 
             $this->RegisterVariableInteger('cases', $this->Translate('Cases'), '', 1);
             $this->RegisterVariableInteger('deaths', $this->Translate('Deaths'), '', 2);
@@ -37,7 +37,16 @@ require_once __DIR__ . '/../libs/COVID19Helper.php';
             $this->RegisterVariableFloat('weekIncidence', $this->Translate('7 Days Incidence'), '', 4);
             $this->RegisterVariableFloat('casesPer100k', $this->Translate('Cases per 100k'), '', 5);
             $this->RegisterVariableInteger('casesPerWeek', $this->Translate('Cases per Week'), '', 6);
-            $this->RegisterVariableFloat('r', $this->Translate('R'), '', 7);
+            $this->RegisterVariableFloat('r', $this->Translate('R Value'), '', 7);
+            $this->RegisterVariableFloat('r4Days', $this->Translate('R Value 4 Days'), '', 8);
+            $this->RegisterVariableFloat('r7Days', $this->Translate('R Value 7 Days'), '', 9);
+
+            $this->RegisterVariableFloat('deltaCases', $this->Translate('Delta Cases'), '', 10);
+            $this->RegisterVariableFloat('deltaDeaths', $this->Translate('Delta Deaths'), '', 11);
+            $this->RegisterVariableFloat('deltaRecovered', $this->Translate('Delta Recovered'), '', 12);
+
+            $this->RegisterVariableFloat('hospitalizationCases7Days', $this->Translate('Hospitalization Cases 7 Days'), '', 13);
+            $this->RegisterVariableFloat('hospitalizationIncidence7Days', $this->Translate('Hospitalization Incidence 7 Days'), '', 14);
 
             if (IPS_GetKernelRunlevel() == KR_READY) {
                 $this->updateCountryStats();
@@ -48,7 +57,7 @@ require_once __DIR__ . '/../libs/COVID19Helper.php';
 
         public function updateCountryStats()
         {
-            $dataJSON = file_get_contents($this->ReadPropertyString('URL').'/germany');
+            $dataJSON = file_get_contents($this->ReadPropertyString('URL') . '/germany');
             $data = json_decode($dataJSON, true);
 
             if (array_key_exists('cases', $data)) {
@@ -59,6 +68,15 @@ require_once __DIR__ . '/../libs/COVID19Helper.php';
                 $this->SetValue('casesPer100k', $data['casesPer100k']);
                 $this->SetValue('casesPerWeek', $data['casesPerWeek']);
                 $this->SetValue('r', $data['r']['value']);
+                $this->SetValue('r4Days', $data['r']['rValue4Days']['value']);
+                $this->SetValue('r7Days', $data['r']['rValue7Days']['value']);
+
+                $this->SetValue('deltaCases', $data['delta']['cases']);
+                $this->SetValue('deltaDeaths', $data['delta']['deaths']);
+                $this->SetValue('deltaRecovered', $data['delta']['recovered']);
+
+                $this->SetValue('hospitalizationCases7Days', $data['hospitalization']['cases7Days']);
+                $this->SetValue('hospitalizationIncidence7Days', $data['hospitalization']['incidence7Days']);
 
                 $this->SetValue('last_update', date('U', strtotime($data['meta']['lastUpdate'])));
             }
